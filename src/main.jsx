8bb9264c310e5c753d199645f3e9bfcb7ccea36c
epsilonnotpstu <ug2102027@cse.pstu.ac.bd>
2026-04-03 22:50:44 +0600

    otp method addition

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./styles.css";
 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
+const isNativeRuntime =
+  typeof window !== "undefined" &&
+  Boolean(window.Capacitor) &&
+  (window.Capacitor?.isNativePlatform?.() ?? false);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {!isNativeRuntime && GOOGLE_CLIENT_ID ? (
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
+    ) : (
+      <App />
+    )}
   </React.StrictMode>,
 );
